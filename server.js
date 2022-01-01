c8a3057ca953c1aeac153f16c4f3b326c6b5ea0b
shubhampawar574 <64591332+shubhampawar574@users.noreply.github.com>
2022-01-01 23:50:57 +0530

    Heroku commit2

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.use("/api/reviews", reviewsRoute);
 if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
